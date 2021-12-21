36f0c176dde1ccbd1559bd974b0831d35a016c2c
sorentfly <sorentfly@mail.ru>
2021-12-22 01:54:43 +0300

    Добавил ссылки в шаблоны, зациклил переходы.
    Реализовал передачу параметра во вьюху.

--- core/core/views.py
+++ core/core/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 
 
+rooms = [
+    {'id': 1, 'name': 'Room 1'},
+    {'id': 2, 'name': 'Room 2'},
+    {'id': 3, 'name': 'Room 3'},
+    {'id': 4, 'name': 'Room 4'},
+]
+
+
 def home(request):
-    return render(request, 'core/home.html')
+    return render(request, 'core/home.html', {'rooms': rooms})
