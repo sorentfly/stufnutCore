690d470d11c96cd5c686675057d76d0048389835
sorentfly <sorentfly@mail.ru>
2021-12-22 01:09:43 +0300

    Реализовал HTML шаблоны

--- core/base/views.py
+++ core/base/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
 
 
 # Create your views here.
 def home(request):
-    return JsonResponse({'msg': 'This is a HomePage'})
+    return render(request, 'base/home.html')
 
 
 def sex(request):
-    return HttpResponse('sex')
+    return render(request, 'base/sex.html')
